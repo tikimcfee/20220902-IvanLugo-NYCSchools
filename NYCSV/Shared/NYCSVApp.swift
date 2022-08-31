1c19b3d5ba95187280d9d99075cf5e313e9d0f6a
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-08-31 15:37:36 -0700

    - Get to codin'

---
+++ NYCSV/Shared/NYCSVApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCSVApp.swift
+//  Shared
+//
+//  Created by Ivan Lugo on 8/31/22.
+//
+
+import SwiftUI
+
+@main
+struct NYCSVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
