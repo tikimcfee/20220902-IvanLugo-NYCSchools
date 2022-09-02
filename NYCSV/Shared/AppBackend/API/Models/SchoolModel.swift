a03828d8d5c8942684dc567d1870e10b25c6fcfb
Ivan Lugo <ivan.lugo@knights.ucf.edu>
2022-09-01 18:17:34 -0700

    = Pull in some fields

--- NYCSV/Shared/AppBackend/API/Models/SchoolModel.swift
+++ NYCSV/Shared/AppBackend/API/Models/SchoolModel.swift
@@ -17,4 +17,46 @@ struct SchoolModel: Hashable, ListDecodable {
     let overview_paragraph: String
     let website: String
     let school_name: String
-}
+    
+    // A slew of interesting optional parameters for display.
+    // We could also use the other mentioned trick and use JSONSerialization
+    // to map directly into these as well.
+    // Run the unit test to pull more as you'd like.
+    let program1: String?
+    let program2: String?
+    let program3: String?
+    
+    let academicopportunities1: String?
+    let academicopportunities2: String?
+    let academicopportunities3: String?
+    
+    let specialized: String?
+    let school_accessibility_description: String?
+    
+    let eligibility1: String?
+    let eligibility2: String?
+    let eligibility3: String?
+    
+    let advancedplacement_courses: String?
+    let school_sports: String?
+    let interest1: String?
+    let interest2: String?
+    let interest3: String?
+    let interest4: String?
+    let interest5: String?
+    
+    let admissionspriority11: String?
+    let admissionspriority12: String?
+    let admissionspriority13: String?
+    
+    let auditioninformation1: String?
+    let auditioninformation2: String?
+    let auditioninformation3: String?
+
+    let directions1: String?
+    let directions2: String?
+    let directions3: String?
+    
+    let fax_number: String?
+    let community_board: String?
+    let school_email: String?}
